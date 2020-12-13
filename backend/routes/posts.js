dfbc22ebc7b543d69040d4118bb478c15e7f753c
Elina Meier <elina.meier@ut.ee>
2020-12-13 20:02:05 +0200

    unlike

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -50,6 +50,12 @@ router.put('/:postId/likes', authorize, (request, response) => {
 router.delete('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to unlike a post
+    let userId = request.currentUser.id
+    let postId = request.params.postId
+
+    PostModel.unlike(userId, postId, () => {
+        response.status(201).json()
+    });
 
 });
 
